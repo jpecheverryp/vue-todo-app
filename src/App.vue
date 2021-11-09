9b2e787a832c55724a57981b6a00b38355ddc078
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-11-08 21:31:42 -0500

    hardcoded tasks data on app

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,33 @@ export default {
   name: 'App',
   components: {
     Header,
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'March 3rd at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'March 13th at 11:00am',
+        reminder: false,
+      }
+    ]
   }
 }
 </script>
